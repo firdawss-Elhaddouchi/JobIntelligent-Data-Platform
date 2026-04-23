55499e741216bfcdb657f4bc5ce0aad83489c461
amina dourdi <amina.dourdi@etu.uae.ac.ma>
2026-04-23 15:00:55 +0100

    adzuna cleaning merging

--- scripts/processing/__init__.py
+++ scripts/processing/__init__.py
@@ -1,7 +1,3 @@
 from scripts.processing.cleaner import clean_arbeitnow_data, clean_adzuna_data, clean_reed_data
 # from scripts.processing.standardizer import standardize_arbeitnow_data, standardize_adzuna_data, standardize_reed_data
-<<<<<<< HEAD
 # from scripts.processing.transformer import transform_arbeitnow_data, transform_adzuna_data, transform_reed_data
-=======
-from scripts.processing.transformer import transform_arbeitnow_data, transform_adzuna_data, transform_reed_data
->>>>>>> eaa48cfb468b415a288d99e52e55f0fa6db94d35
