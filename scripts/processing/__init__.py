9f41df42e7ecb5a4e5d6b69fd93f2e50b237b751
oumaima <elghalbouniomaima@gmail.com>
2026-04-24 04:12:14 +0200

    fix merge error

--- scripts/processing/__init__.py
+++ scripts/processing/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# from scripts.processing.cleaner import clean_arbeitnow_data, clean_adzuna_data, clean_reed_data
-=======
 from scripts.processing.cleaner import clean_arbeitnow_data, clean_adzuna_data, clean_reed_data
->>>>>>> 0c0f7aebf240f43d3c3a054acd0c937415be43d4
 # from scripts.processing.standardizer import standardize_arbeitnow_data, standardize_adzuna_data, standardize_reed_data
 # from scripts.processing.transformer import transform_arbeitnow_data, transform_adzuna_data, transform_reed_data
